64b55436f1630493eeee1e257f48cbcd7d1d7930
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2026-08-18 16:12:51 +0200

    update typescript

--- packages/dactylo/tsup.config.ts
+++ packages/dactylo/tsup.config.ts
@@ -2,18 +2,6 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   clean: true,
-  dts: {
-    /**
-     * Ignore deprecations for TypeScript 6+ 👉🏻 workaround to fix the build
-     * error with TypeScript 6+ when getting the deprecation error: - "error
-     * TS5101: Option 'baseUrl' is deprecated and will stop functioning in
-     * TypeScript 7.0. Specify compilerOption '"ignoreDeprecations": "6.0"' to
-     * silence this error." - https://github.com/egoist/tsup/issues/1388
-     */
-    compilerOptions: {
-      ignoreDeprecations: '6.0',
-    },
-  },
   entry: [
     /** main package entry point */
     'src/index.ts',
