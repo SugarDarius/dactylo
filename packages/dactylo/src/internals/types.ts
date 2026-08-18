f1f4ea5fde84acf586f0fbf4c3adb6ea79941e3b
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2026-08-18 16:20:03 +0200

    add types file

---
+++ packages/dactylo/src/internals/types.ts
@@ -0,0 +1,42 @@
+/**
+ * Allow for an operation to return either
+ * a value or a promise of a value.
+ */
+export type Awaitable<T> = T | PromiseLike<T>
+
+/**
+ * Forces TypeScript to "evaluate" named helper types, making API signatures
+ * clearer in IDEs.
+ *
+ * @see https://effectivetypescript.com/2022/02/25/gentips-4-display/
+ */
+type Resolve<T> = T extends (...args: unknown[]) => unknown
+  ? T
+  : { [K in keyof T]: T[K] }
+
+// oxlint-disable-next-line typescript/no-explicit-any
+type DistributiveRelax<T, Ks extends string | number | symbol> = T extends any
+  ? Resolve<
+      { [K in keyof T]: T[K] } & Partial<Record<Exclude<Ks, keyof T>, never>>
+    >
+  : never
+
+/**
+ * Relaxes a discriminated union type definition, by explicitly adding
+ * properties defined in any other member as optional `never`.
+ *
+ * This makes accessing the members much more relaxed in TypeScript.
+ *
+ * Thanks to https://github.com/nvie/decoders/blob/main/src/lib/Relax.ts
+ * for the original implementation of the `Relax` type 👇🏻
+ */
+// oxlint-disable-next-line typescript/no-explicit-any
+export type Relax<T> = DistributiveRelax<T, T extends any ? keyof T : never>
+
+/**
+ * Enables autocompletion for a union type, while keeping the ability to use any string
+ * or type of `T`
+ */
+export type Autocomplete<U extends T, T = string> =
+  | U
+  | (T & Record<never, never>)
