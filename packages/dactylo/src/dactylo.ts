f8be1336a5be80154c6f0f086e908543e1f7cbea
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2026-08-19 12:57:48 +0200

    add document initialization

--- packages/dactylo/src/dactylo.ts
+++ packages/dactylo/src/dactylo.ts
@@ -1,4 +1,6 @@
 import { DEFAULT_PLACEHOLDER } from './internals/constants'
+import { createInitialEmptyDocState } from './internals/document'
+import type { DocState } from './internals/document'
 
 /** Options for constructing a {@link Dactylo} instance. */
 export interface DactyloProps {
@@ -10,7 +12,24 @@ export class Dactylo {
   /** Placeholder text for the editor when no content is written. */
   readonly placeholder: string
 
+  /** Current document state */
+  #state: DocState
+
   constructor(options: DactyloProps) {
     this.placeholder = options.placeholder ?? DEFAULT_PLACEHOLDER
+    /**
+     * Initialize the document state.
+     * If no initial content is provided, it creates an empty document
+     * with a placeholder.
+     *
+     * Otherwise initial document state is provider from either:
+     *  - a JSON object (validated against the current schema version)
+     *  - a markdown string
+     *
+     * TODO: handle initial content from props
+     *  - from JSON
+     *  - from markdown string
+     */
+    this.#state = createInitialEmptyDocState(this.placeholder)
   }
 }
