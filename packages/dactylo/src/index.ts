a23b0aebd856f07585e964eb13b50263066cd1d4
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2026-09-09 19:45:24 +0200

    add some exports in index.ts

--- packages/dactylo/src/index.ts
+++ packages/dactylo/src/index.ts
@@ -1,1 +1,6 @@
 export { Dactylo, type DactyloOptions } from './dactylo'
+export type {
+  EditorContext,
+  EditorContextListener,
+} from './internals/editor-context'
+export type { Marks, MarkKey } from './internals/marks'
